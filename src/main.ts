99dba3d6f683d6ca8da90947092c823f126ffdbb
anvarbeck <anvarsaidov732@gmail.com>
2025-12-22 11:37:33 +0500

    config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-
-
 import express from 'express';
-
 
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res, next) => {
+    res.send('Hello, World!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
